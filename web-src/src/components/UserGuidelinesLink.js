b286663ad8a1878dd6328c08e601c2e99b02ad48
Ronald E. Oribio R <1751465+roribio@users.noreply.github.com>
2023-11-17 12:03:52 +0000

    Update User Guidelines Link

--- web-src/src/components/UserGuidelinesLink.js
+++ web-src/src/components/UserGuidelinesLink.js
@@ -12,7 +12,7 @@
 import { Link } from '@adobe/react-spectrum';
 import React from 'react';
 
-const USER_GUIDELINES_URL = 'https://www.adobe.com/legal/licenses-terms/adobe-gen-ai-user-guidelines.html';
+const USER_GUIDELINES_URL = 'https://www.adobe.com/legal/licenses-terms/adobe-dx-gen-ai-user-guidelines.html';
 
 export function UserGuidelinesLink() {
   return (
